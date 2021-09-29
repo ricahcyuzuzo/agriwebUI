23e9e81420eae34e7ac4110845eb22b6993e53c1
ricahcyuzuzo <ricahcyuzuzo@gmail.com>
2021-09-29 12:37:25 +0200

    Add data

--- src/components/ProductRowOne.js
+++ src/components/ProductRowOne.js
@@ -8,7 +8,7 @@ import { FaArrowLeft } from 'react-icons/fa';
 import axios from 'axios';
 
 function Farmers(){
-    const [data, setData] = useState({});
+    const [data, setData] = useState([]);
     const [show, setShow] = useState(false);
     const [names, setNames] = useState('');
     const [phone, setPhone] = useState('');
@@ -30,7 +30,8 @@ function Farmers(){
 
     useEffect(() => {
         axios.get('https://agrirwanda.eu-4.evennode.com/api/users', {params: {telephone: window.localStorage.getItem('phone')}}).then((res) => {
-            setData(res.data.data[0]);
+            setData(res.data.data);
+            console.log(data)
         })
     }, [])
 
@@ -49,41 +50,45 @@ function Farmers(){
                 marginBottom: 20
             }}><FaArrowLeft size={24} /></Button>
             </div>
-            <div className='shadow-lg p-3 mb-5 bg-body' style={{
-                width: '100%',
-                height: 140,
-                display: 'flex',
-                justifyContent: 'space-between',
-                borderRadius: 15
-            }}>
-                <div style={{
+            {
+                data.length !== 0 ? <div className='shadow-lg p-3 mb-5 bg-body' style={{
+                    width: '100%',
+                    height: 140,
                     display: 'flex',
+                    justifyContent: 'space-between',
+                    borderRadius: 15
                 }}>
-                    <img style={{
-                        width: 80,
-                        height: 80,
-                        borderRadius: '100%',
-                    }} alt='product' src='https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small/Basic_Ui__28186_29.jpg' />
+                    <div style={{
+                        display: 'flex',
+                    }}>
+                        <img style={{
+                            width: 80,
+                            height: 80,
+                            borderRadius: '100%',
+                        }} alt='product' src='https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small/Basic_Ui__28186_29.jpg' />
+                        <div style={{
+                            marginTop: 10,
+                            marginLeft: 20
+                        }}>
+                            <h4>{data[0]?.fullNames}</h4>
+                            <p><Badge variant='primary'>{window.localStorage.getItem('quantity')} KGs</Badge> <Badge variant='dark' style={{
+                                backgroundColor: 'purple'
+                            }}>{window.localStorage.getItem('price')} RWF / Kg</Badge></p>
+                        </div>
+                    </div>
                     <div style={{
                         marginTop: 10,
-                        marginLeft: 20
+                        textAlign: 'right',
+                        color: 'gray'
                     }}>
-                        <h4>{data?.fullNames}</h4>
-                        <p><Badge variant='primary'>{window.localStorage.getItem('quantity')} KGs</Badge> <Badge variant='dark' style={{
-                            backgroundColor: 'purple'
-                        }}>{window.localStorage.getItem('price')} RWF / Kg</Badge></p>
+                        <h5>+25{window.localStorage.getItem('phone')}</h5>
+                        <h6>{data[0]?.address.province}/{data[0]?.address.district}/{data[0]?.address.sector}/{data[0]?.address.cell}/{data[0]?.address.village}</h6>
+                        <button className='btn btn-primary' onClick={handleShow} >Send Message</button>
                     </div>
-                </div>
-                <div style={{
-                    marginTop: 10,
-                    textAlign: 'right',
-                    color: 'gray'
-                }}>
-                    <h5>+25{window.localStorage.getItem('phone')}</h5>
-                    <h6>{data?.address.province}/{data?.address.district}/{data?.address.sector}/{data?.address.cell}/{data?.address.village}</h6>
-                    <button className='btn btn-primary' onClick={handleShow} >Send Message</button>
-                </div>
-            </div>
+                </div> : <h2 style={{
+                    fontFamily: 'Urbanist'
+                }}>Loading ...</h2>
+            }
             
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
